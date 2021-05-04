80a89b233fb16711269fb08e0804e41b46c99ac0
Danielle Gerardi <dgerardi@nd.edu>
2021-05-04 16:46:47 -0400

    predict.py

---
+++ ngram/predict.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+#Program takes in at least one file in the command line that has history data formatted with the date and time. Calls the Ngram class to build a dictionary of probabilities based on this data. After inputting at least n (2) commands, runs until the user enters q. Using the ngram probabilities, predicts the first word of the succeeding command after every new command is entered. The user can enter n to delete the prediction or continue typing the command to accept it. 
+
+import sys 
+from ngram_class import make_list
+from ngram_class import word_test
+from ngram_class import Ngram
+
+def main():
+
+	n = 2;
+	probabilities = Ngram()
+	data = []
+	commands = []
+	arguments = sys.argv[1:]
+	count = 0
+
+	while arguments:
+		argument = arguments.pop(0)
+		data = make_list(data, argument)
+
+	#Create probabilities dictionary based on data from history files inputted in the command line
+	probabilities.word_ngram(n, data)
+
+	print("Enter two commands to start recieving command predictions. Enter n to delete prediction. Enter q to quit.")
+	#Create inital list of n commands to base first prediction off of 
+	while count < n:
+		line = input()
+		if line.strip() == "q":
+			exit()
+		commands.append(line.rstrip().split()[0])
+		count += 1
+
+	#Use word test to get prediction
+	prediction = word_test(n, commands, probabilities.prob_dict)
+	#If there is no prediction, clear the line and have the next prediction be based on the next line inputted (I feel like this block could be a function since it repeats later but I couldn't figure out how)
+	while prediction == None:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[K")
+			line = input()
+			prediction = line.rstrip().split()[0]
+			commands.append(prediction)
+			commands.pop(0)
+			prediction = word_test(n, commands, probabilities.prob_dict)
+	#If there is a prediction, prompt the user with the prediction and allow them to type the rest of the command
+	else:
+		line = input(prediction)
+
+	while 1:
+		if line.strip() == "q":
+			exit()
+		#When the user enters n, clear the line and have the next prediction be based on the next line inputted 
+		if line.strip() == "n":
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[K")
+			line = input()
+			prediction = line.rstrip().split()[0]
+		#Add most recent command to the list, and remove oldest command. Make a new prediction based on this new list of commands
+		commands.append(prediction)
+		commands.pop(0)
+		prediction = word_test(n, commands, probabilities.prob_dict)
+		while prediction == None:
+			sys.stdout.write("\033[F")
+			sys.stdout.write("\033[K")
+			line = input()
+			prediction = line.rstrip().split()[0]
+			commands.append(prediction)
+			commands.pop(0)
+			prediction = word_test(n, commands, probabilities.prob_dict)
+		else:
+			line = input(prediction)
+
+if __name__ == '__main__':
+	main()
+		
+		
+		 
