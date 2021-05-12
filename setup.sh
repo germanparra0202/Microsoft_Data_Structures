27de3ea095805ffed13413f8a60c844de5fc8c0f
Coleen Gillilan <cgillila@nd.edu>
2021-05-12 11:38:26 -0400

    setup file

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# README.txt
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/README.txt
+
+# initialize.py
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/initialize.py
+chmod +x initialize.py
+
+# login.py
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/login.py
+chmod +x login.py
+
+# login_funcs.py
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/login_funcs.py
+chmod +x login_funcs.py
+
+# predict.py
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/predict.py
+chmod +x predict.py
+
+# ngram_class.py
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/ngram_class.py
+chmod +x ngram_class.py
+
+# history files
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/history.txt
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/history_cg.txt
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/history2_cg.txt
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/history_dg.txt
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/history2_dg.txt
+
+# get_history
+wget https://raw.githubusercontent.com/matt-schoessling/Microsoft-Data-Structures-Project/main/Recommendation/get_history.py
+chmod +x get_history.py
+
